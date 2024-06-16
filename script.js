9dc888ea84bc60fac0fd9f2f59ce8624fc1f8e41
mpreams <mreams97@gmail.com>
2024-06-16 02:48:07 -0400

    Add 16x16 grid with hover effect

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-container = document.querySelector('.container');
+let container = document.querySelector('.container');
+container.setAttribute('style', 'display: flex');
+
+for (var i = 0; i < 16; i++) {
+    let row = document.createElement('div');
+    row.setAttribute('id', i);
+    container.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+        let square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.setAttribute('style', 'border: 1px solid black; margin: 10px 5px; height: 20px; width: 20px');
+        square.addEventListener('mouseenter', () => square.style.backgroundColor = 'blue');
+        row.appendChild(square);
+    }
+}
